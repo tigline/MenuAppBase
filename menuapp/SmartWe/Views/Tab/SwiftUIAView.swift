cdf54adbcb8f9313c9a66380ed8e212a5655ff59
tigline <projectgame@163.com>
2024-02-19 23:59:36 +0900

    LinearGradient for backgroud

--- menuapp/SmartWe/Views/Tab/SwiftUIAView.swift
+++ menuapp/SmartWe/Views/Tab/SwiftUIAView.swift
@@ -22,20 +22,20 @@ enum TestCategory: String, CaseIterable, Identifiable {
 struct SwiftUIAView: View {
     var body: some View {
         NavigationStack {
-            ZStack {
-                // 设置渐变或图像背景
-                LinearGradient(gradient: Gradient(colors: [.blue, .green]), startPoint: .top, endPoint: .bottom)
-                    .edgesIgnoringSafeArea(.all)
-
+      
+                
                 List(TestCategory.allCases, id: \.self) { category in
                     NavigationLink(value: category) {
                         Text(category.localizedName)
                     }
                     .listRowBackground(Color.clear) // 为每行设置透明背景
                 }
-                .background(.pink)
+                .background{
+                    LinearGradient(gradient: Gradient(colors: [.blue, .green]), startPoint: .top, endPoint: .bottom)
+                        .edgesIgnoringSafeArea(.all)
+                }
                 .scrollContentBackground(.hidden)// 确保 List 背景透明
-            }
+       
             .navigationDestination(for: TestCategory.self) { category in
                 // 根据类别展示不同的视图
                 DetailView(category: category)
